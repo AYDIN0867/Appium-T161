4371e9fe2d37b59c4f1361ce235fea8bcaea25e8
ramazan <coolstoves@gmail.com>
2025-10-21 21:12:31 +0300

    first commit

---
+++ src/test/java/tests/day01/ApkYukle.java
@@ -0,0 +1,44 @@
+package tests.day01;
+
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.AndroidElement;
+import io.appium.java_client.remote.MobileCapabilityType;
+import org.junit.Test;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.concurrent.TimeUnit;
+
+public class ApkYukle {
+
+    AndroidDriver<AndroidElement>driver;
+
+    //BU CLASS LARI OLUŞTURURKEN EMULATORDEN OLUŞTURULAN TELEFON
+    // VE APPIUM SERVER AÇIK OLMALI ONCELİKLİ OLARAK
+//APK nın ismi Turkçe karakter içermemeli ve noktadan sonra boşluk olmamalı
+//yani şöyle olmalı Calculator_8.4 (503542421)_Apkpure (3).apk bu doğru bir kullanım
+
+    @Test
+    public void ApkYukle() throws MalformedURLException {
+
+        //buraya testler yazılacak ama class seviyesinde hemen driver oluşturulu....
+        DesiredCapabilities cap=new DesiredCapabilities();
+       cap.setCapability(MobileCapabilityType.DEVICE_NAME,"Pixel 4-T160");
+        cap.setCapability(MobileCapabilityType.PLATFORM_NAME,"Android");
+        cap.setCapability(MobileCapabilityType.PLATFORM_VERSION,"10.0");
+       cap.setCapability(MobileCapabilityType.AUTOMATION_NAME,"UiAutomator2");
+       // cap.setCapability(MobileCapabilityType.APP,"C:\\Users\\Hp\\IdeaProjects\\Team161_Appium\\Apss\\Calculator_8.4 (503542421)_Apkpure (3).apk");
+        cap.setCapability(MobileCapabilityType.APP,"C:\\Users\\Hp\\IdeaProjects\\Team161_Appium\\Apss\\Apk Bilgisi_2.3.4_apkcombo.com.apk");
+
+////27 satırdaki desiredcap i test yapacağımız uygulamayı telefona yüklemek için yaptık...
+//yüklü ise açer yüklü değilse yükler çok güzel bir özellik yani süper...
+
+//***************************ŞİMDİ DRİVER AYARLARINA GELİYORUZ..
+
+        driver = new AndroidDriver<>(new URL("http://127.0.0.1:4723/wd/hub"), cap);// http://127.0.0.1:4723/wd/hub bu appium server ından gelen yoldur
+        driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+
+
+    }
+}
---
+++ src/test/java/tests/day01/HesapMak.java
@@ -0,0 +1,56 @@
+package tests.day01;
+
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.AndroidElement;
+import io.appium.java_client.remote.MobileCapabilityType;
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.concurrent.TimeUnit;
+
+public class HesapMak {
+
+//uygulamanın yuklendigini dogrular
+//uygulamanın acıldıgını dogrular
+//
+
+    AndroidDriver<AndroidElement> driver;
+    //onceden tek bir driver vardı.o da appiumDriver di. zamanla appium kendini geliştirdi
+    //android için ayrı ios icin ayrı özellikleri bulunan driverlar uretti.IOSDriver<IOSElement>iosDriver;
+
+    @Test
+    public void HesapMak() throws MalformedURLException {
+        // kullanici gerekli kurulumu yapar
+
+        DesiredCapabilities cap=new DesiredCapabilities();
+      cap.setCapability(MobileCapabilityType.DEVICE_NAME,"Pixel 4-T160");
+       cap.setCapability(MobileCapabilityType.PLATFORM_NAME,"Android");
+       cap.setCapability(MobileCapabilityType.PLATFORM_VERSION,"10.0");
+        cap.setCapability(MobileCapabilityType.AUTOMATION_NAME,"UiAutomator2");
+        cap.setCapability(MobileCapabilityType.APP,"C:\\Users\\Hp\\IdeaProjects\\Team161_Appium\\Apss\\Calculator_8.4 (503542421)_Apkpure (3).apk");
+
+//27 satırdaki desiredcap i test yapacağımız uygulamayı telefona yüklemek için yaptık...
+//yüklü ise açar yüklü değilse yükler çok güzel bir özellik yani süper...
+
+//***************************ŞİMDİ DRİVER AYARLARINA GELİYORUZ..
+
+        driver = new AndroidDriver<>(new URL("http://127.0.0.1:4723/wd/hub"), cap);// http://127.0.0.1:4723/wd/hub bu appium server ından gelen yoldur
+        driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+
+//uygulamanın yuklendigini dogrular(isInstalled)
+        Assert.assertTrue(driver.isAppInstalled("com.google.android.calculator"));
+//bundleid bulunacak
+
+//uygulamanın açıldıgı dogrulanır
+
+
+
+
+
+    }
+
+
+}
